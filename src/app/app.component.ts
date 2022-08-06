3e133ccb2bc2cc616213f39677f469380d710152
LuanCalais <luancalais@gmail.com>
2022-08-06 17:04:49 -0300

    criar eventos demonstrando chamadas de eventos de ciclo de vida de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-principal [valorInicial]="valor"></app-principal>
+  template: `
+  <button (click)="mudarValor()">Mudar Valor</button>
+  <button (click)="destruirCiclo()">Destruir</button>
+  <app-principal [valorInicial]="valor" *ngIf="!deletarCiclo"></app-principal>
   `,
   styles: []
 })
 export class AppComponent {
+
+  // Valor em property binding
+  // Efetuamos o incremento atiavando alterações no compoenente
+  // São disparados - ngOnChanges, ngDoCheck, ngAfterContentChecked e ngAfterViewChecked
   valor: number = 5;
+  mudarValor(){
+    this.valor++;
+  }
+
+  // Variável booleana que controla o aparecimento do componente <app-principal> com o ngIf
+  // Assim que o ngIf for false, o que removera o carregamento do componente 
+  // o ngOnDestroi() é chamado
+  deletarCiclo: boolean = false;
+  destruirCiclo(){
+     this.deletarCiclo = true;
+  }
+
 }
